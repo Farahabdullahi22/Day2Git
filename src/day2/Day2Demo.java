0482e60fbe9b45c680390826f76c0757a28e94fe
farahabdullahi <farahabdullahi@192.168.4.25>
2023-04-07 21:20:00 -0400

    multiple method is implemented

--- src/day2/Day2Demo.java
+++ src/day2/Day2Demo.java
@@ -1,12 +1,30 @@
 package day2;
 
 public class Day2Demo {
-	
-	public static void main(String[] args) {
-		
 
-	System.out.println("Day 2 ");
-	
+	public static void main(String[] args) {
+
+		System.out.println("Day 2 git");
+
+	}
+
+	public void multiple(int a, int b) {
+
+		System.out.println(a * b);
+
+		System.out.println("Day 2 ");
+
+	}
+
+	public void multiple1(int a, int b) {
+
+		System.out.println(a * b * 3);
+
+	}
+
+	public void multiple2(int a, int b) {
+
+		System.out.println(a * b * 3 * 4);
 
 	}
 
